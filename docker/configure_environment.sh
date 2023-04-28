befcdd154d7ff6a8bdb0bf08a7befba910842d3b
Hubert Strautz <hustdev@hotmail.com>
2023-04-28 08:56:00 +0200

    update environment configuration

--- docker/configure_environment.sh
+++ docker/configure_environment.sh
@@ -21,6 +21,7 @@ copy_template() {
 # 2 base dir
 # 3 is in dev env
 # 4 path to icecast config file
+# 5 domain node application
 configure_env () {
     case $3 in
         [Yy]* ) sed -i "s#_NPM_SKIP_START_#NPM_SKIP_START=1#g" "$1";;
@@ -29,27 +30,46 @@ configure_env () {
 
     host_dir="$(dirname "$2")/website"
     sed -i "s#_NODE_HOST_DIR_#$host_dir#g" "$1"
-
     sed -i "s#_ICECAST_CONFIG_#$4#g" "$1"
+    sed -i "s#_DOMAIN_NODE_APP_#$5#g" "$1"
 
-    update_placeholder "Rocket.Chat version (proposed 5.4.8):" "_ROCKET_CHAT_" "$1"
-    update_placeholder "MongoDB version (proposed 4.4.9):" "_MONGO_DB_" "$1"
-    update_placeholder "Traefik version (v2.9.10):" "_ROCKET_CHAT_" "$1"
-    update_placeholder "Email that shall be used for ACME:" "_ACME_EMAIL_" "$1"
+    update_placeholder "(Docker): Email that shall be used for ACME:" "_ACME_EMAIL_" "$1"
 }
 
 ########################################
 # 1 path to icecast cfg
+# 2 domain streaming server
+# 3 domain node application
 configure_icecast () {
-    update_placeholder "Location of server:" "_LOCATION_" "$1"
-    update_placeholder "Admin email (and name):" "_CONTACT_" "$1"
-    update_placeholder "URL of icecast host (if run in dev env with docker use ip of docker ethernet adapter):" "_HOSTNAME_" "$1"
-    update_placeholder "Source password:" "_SOURCE_PASSWORD_" "$1"
-    update_placeholder "Relay password:" "_RELAY_PASSWORD_" "$1"
-    update_placeholder "Admin username:" "_ADMIN_USERNAME_" "$1"
-    update_placeholder "Admin password:" "_ADMIN_PASSWORD_" "$1"
-    update_placeholder "Authentication url:" "_AUTH_URL_" "$1"
-    update_placeholder "Log level (4 Debug, 3 Info, 2 Warn, 1 Error):" "_LOG_LEVEL_" "$1"
+    update_placeholder "(Icecast): Location of server:" "_LOCATION_" "$1"
+    update_placeholder "(Icecast): Admin email (and name):" "_CONTACT_" "$1"
+    update_placeholder "(Icecast): Source password:" "_SOURCE_PASSWORD_" "$1"
+    update_placeholder "(Icecast): Relay password:" "_RELAY_PASSWORD_" "$1"
+    update_placeholder "(Icecast): Admin username:" "_ADMIN_USERNAME_" "$1"
+    update_placeholder "(Icecast): Admin password:" "_ADMIN_PASSWORD_" "$1"
+    update_placeholder "(Icecast): Log level (4 Debug, 3 Info, 2 Warn, 1 Error):" "_LOG_LEVEL_" "$1"
+
+    sed -i "s#_HOSTNAME_#$2#g" "$1"
+    sed -i "s#_AUTH_URL_#$3/radio#g" "$1"
+}
+
+########################################
+# 1 website dir
+# 2 domain chat
+# 3 domain stream
+configure_node () {
+    copy_template "$1/backend/config.js.in" "$1/backend/config.js"
+
+    sed -i "s#_DOMAIN_CHAT_#$2#g" "$1/backend/config.js"
+    sed -i "s#_DOMAIN_STREAM_#$3#g" "$1/backend/config.js"
+
+    update_placeholder "(Node): Secret key for encryption (32 chars):" "_SECRET_KEY_" "$1/backend/config.js"
+    update_placeholder "(Node): Quick validation key:" "_VALIDATION_KEY_" "$1/backend/config.js"
+
+    copy_template "$1/backend/config.js" "$1/frontend/config.js"
+
+    sed -i "s#_EXPORT_#export #g" "$1/backend/config.js"
+    sed -i "s#_EXPORT_##g" "$1/frontend/config.js"
 }
 
 ################################################################################
@@ -59,17 +79,25 @@ configure_icecast () {
 echo "Used in VSCode development environment (y/n)?"
 read is_dev_env
 
-base_dir="$(realpath "$BASH_SOURCE")"
-base_dir="$(dirname "$base_dir")"
+docker_dir="$(realpath "$BASH_SOURCE")"
+docker_dir="$(dirname "$docker_dir")"
 
-env_file="$base_dir/.env"
+env_file="$docker_dir/.env"
+icecast_cfg="$docker_dir/icecast.xml"
 
-echo "# Path where to save icecast.xml configuration?"
-read icecast_cfg
-icecast_cfg="$icecast_cfg/icecast.xml"
+copy_template "$docker_dir/env.template" "$env_file"
+copy_template "$docker_dir/icecast.xml.in" "$icecast_cfg"
 
-copy_template "$base_dir/env.template" "$env_file"
-copy_template "$base_dir/icecast.xml.in" "$icecast_cfg"
+website_dir="$(dirname "$docker_dir")"
+website_dir="$website_dir/website"
 
-configure_env "$env_file" "$base_dir" "$is_dev_env" "$icecast_cfg"
-configure_icecast "$icecast_cfg"
+echo "Domain of node application:"
+read domain_node
+echo "Domain of streaming server:"
+read domain_streaming
+echo "Domain of chat server:"
+read domain_chat
+
+configure_env "$env_file" "$docker_dir" "$is_dev_env" "$icecast_cfg" "$domain_node"
+configure_icecast "$icecast_cfg" "$domain_streaming" "$domain_node"
+configure_node "$website_dir" "$domain_node" "$domain_streaming"
