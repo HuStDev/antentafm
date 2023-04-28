e1fc7819a324a4a8eb01f724e331ff254ce354c2
Hubert Strautz <hustdev@hotmail.com>
2023-04-28 12:14:40 +0200

    fix typo in script

--- docker/stop_docker.sh
+++ docker/stop_docker.sh
@@ -3,5 +3,5 @@ base_dir="$(realpath "$BASH_SOURCE")"
 base_dir="$(dirname "$base_dir")"
 
 pushd $base_dir
-docker-compose -f docker-compose.node.yml -f docker-compose.rocketchat.yml -f docker-compose.icecast.yml -f docker-compose.traefik.yml up
+docker-compose -f docker-compose.node.yml -f docker-compose.rocketchat.yml -f docker-compose.icecast.yml -f docker-compose.traefik.yml down
 popd
